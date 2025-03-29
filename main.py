7b27beb4a7dde763f7dbac36a99592fff7d2d207
DCaraballo-design <artistdmc@gmail.com>
2025-03-29 17:01:16 -0400

    book bot can count

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def main():
+     from stats import word_count
      file_path = "books/frankenstein.txt"
      text = get_book_text(file_path)
-     print(text)
+     num_words = len(word_count(text))
+     print(f"{num_words} words found in the document")
 
 def get_book_text(path):
     # Use an absolute path to the file
