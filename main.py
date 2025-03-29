790676c89e7a3f9a58dd4657c6f214976259fda2
DC <artistdmc@gmail.com>
2025-03-29 15:42:25 -0400

    remember to specify encoding if necessary

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+text = ""
+
+def get_book_text():
+    # Use an absolute path to the file
+    file_path = r"D:\artis\Documents\Bootbot\bootbot\books\frankenstein.txt"
+    with open(file_path, "r", encoding='utf-8') as file:
+        content = file.read()
+        print(content)
+
+def main():
+    # Call the function to get the book text
+    get_book_text()
+
+main()
