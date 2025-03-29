427113b9fb8ba9521affe2818d696440fceb1b61
DCaraballo-design <artistdmc@gmail.com>
2025-03-29 16:35:43 -0400

    updated to follow with boo.dev

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-text = ""
-
-def get_book_text():
-    # Use an absolute path to the file
-    file_path = r"books/frankenstein.txt"
-    with open(file_path, "r", encoding='utf-8') as file:
-        content = file.read()
-        print(content)
-
 def main():
-    # Call the function to get the book text
-    get_book_text()
+     file_path = "books/frankenstein.txt"
+     text = get_book_text(file_path)
+     print(text)
 
+def get_book_text(path):
+    # Use an absolute path to the file
+    with open(path, "r", encoding='utf-8') as file:
+        return file.read()
+   
 main()
